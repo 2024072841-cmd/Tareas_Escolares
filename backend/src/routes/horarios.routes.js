aeab9fe8a9d1b06e33d9618eb77b0b611098f08e
Mayra Liliana <2024072841@upb.edu.mx>
2026-03-12 11:08:27 -0500

    cambios

--- backend/src/routes/horarios.routes.js
+++ backend/src/routes/horarios.routes.js
@@ -1,7 +1,31 @@
+/*
+Endpoints
+http://localhost:3000/api/horarios/     metodo: POST   nuevo horario
+http://localhost:3000/api/horarios/materia/id/   metodo: GET   consultar materia
+http://localhost:3000/api/horarios/     metodo: GET   consultar horarios
+http://localhost:3000/api/horarios/id/     metodo: PUT   actualizar horario con id
+http://localhost:3000/api/horarios/id/     metodo: DELETE   eliminar horario con id
+*/
+
+
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/horarios.controller');
 
+// Crear nuevo horario
+router.post('/', verificarToken, controller.crearHorario);
 
-// rutas se agregarán después
+// Consultar horarios de una materia en particular (id_materia)
+router.get('/materia/:id_materia', verificarToken, controller.obtenerHorariosPorMateria);
+
+// Listar los horarios por materias
+router.get('/', verificarToken, controller.obtenerHorarioCompleto);
+
+// Actualizar horaio
+router.put('/:id', verificarToken, controller.actualizarHorario);
+
+// Eliminar horario
+router.delete('/:id', verificarToken, controller.eliminarHorario);
 
 module.exports = router;
--- backend/src/routes/tareas.routes.js
+++ backend/src/routes/tareas.routes.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/tareas.controller');
 
+/*
+Endpoints.
+http://localhost:3000/tareas/  metodo: POST    nueva tarea
+http://localhost:3000/tareas/  metodo: POST    listar todas las tareas
+http://localhost:3000/tareas/id  metodo: GET   listar 1 tarea
+http://localhost:3000/tareas/id  metodo: PUT   actualizar tarea
+http://localhost:3000/tareas/id/completar  metodo: PATCH   cambiar estado
+http://localhost:3000/tareas/id  metodo: DELETE   borrar tarea
+http://localhost:3000/tareas/estado/pendiente   metodo GET
+http://localhost:3000/tareas/estado/vencida     metodo GET
+http://localhost:3000/tareas/estado/completada  metodo GET
+*/
 
-// rutas se agregarán después
+// crear una nueva tarea
+router.post('/', verificarToken, controller.crearTarea);
+
+// consultar todas las treas
+router.get('/', verificarToken, controller.obtenerTodasLasTareas);
+
+// consultar una tarea según su id
+router.get('/:id', verificarToken, controller.obtenerTareaPorId);
+
+// actualizar una tarea
+router.put('/:id', verificarToken, controller.actualizarTarea);
+
+// marcar como comletada una tarea
+router.patch('/:id/completar', verificarToken, controller.marcarComoCompletada);
+
+// eliminar una tarea por id
+router.delete('/:id', verificarToken, controller.eliminarTarea);
+
+// Endpoints adicinales
+// tareas pendientes
+router.get('/estado/pendientes', verificarToken, controller.tareasPendientes);
+
+// tareas vencidas
+router.get('/estado/vencidas', verificarToken, controller.tareasVencidas);
+
+// tareas completadas
+router.get('/estado/completadas', verificarToken, controller.tareasCompletadas);
 
 module.exports = router;
